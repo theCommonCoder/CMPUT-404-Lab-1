c57c97e1ab036849484f8cb196f56e430c03c7b9
Mason <mdbly@ualberta.ca>
2023-09-11 11:14:17 -0600

    Update lab1.py to print its own code

--- lab1.py
+++ lab1.py
@@ -1,2 +1,8 @@
 import requests
-print(requests.__version__)
+
+print("requests version:", requests.__version__)
+
+r = requests.get('https://raw.githubusercontent.com/theCommonCoder/CMPUT-404-Lab-1/master/lab1.py')
+
+print()
+print(r.content.decode("utf8"))
