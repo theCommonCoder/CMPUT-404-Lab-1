74c62dfbd7982d058f7ee97dedebc932e7b89f83
Mason <mdbly@ualberta.ca>
2023-09-11 17:25:04 -0600

    Download and get own source code

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@ import requests
 
 print("requests version:", requests.__version__)
 
-r = requests.get('https://www.google.com')
+r = requests.get('https://raw.githubusercontent.com/theCommonCoder/CMPUT-404-Lab-1/master/lab1.py')
 
 print()
 print(r.content.decode("utf8"))
